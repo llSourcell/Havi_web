5cbeea53fe30c2287d8da6a56d2c348267344a16
Siraj Ravel <jason.ravel@cbsinteractive.com>
2014-12-14 21:07:45 -0800

    lol

--- app.js
+++ app.js
@@ -1008,7 +1008,6 @@ else {
 app.post('/collectpaymentdata', function(req, res){
 	
 	console.log('the req', req.body.stripeToken);
-	console.log('the user1', the_user.id);
 	console.log('the user1', req.user.id);
 	
 	
